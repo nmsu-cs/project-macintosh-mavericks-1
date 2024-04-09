d9669bd619202875a9bfb4d4b40980c81ab24409
Alex <78886791+alexander2475@users.noreply.github.com>
2024-04-09 13:51:21 -0600

    added buttons functionality and responsive grid layout

--- TaskQuest.java
+++ TaskQuest.java
@@ -1,26 +1,26 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TaskQuest extends JPanel {
 
+    private JPanel taskPanel;
+
     public TaskQuest() {
-        
-        //! Setting frame layout type to BorderLayout
+
+        //Sets layout type to BorderLayout
         setLayout(new BorderLayout());
+
         this.add(this.taskUISetup(), BorderLayout.CENTER);
-        this.playerBar();
+        this.add(this.playerBar(), BorderLayout.SOUTH);
     }
 
     private JPanel taskUISetup(){
-        JPanel taskPanel = new JPanel();
-        taskPanel.setLayout(new GridLayout(2, 2, 20, 20));
+        taskPanel = new JPanel();
+        taskPanel.setLayout(new GridLayout(0, 1, 20, 20));
         taskPanel.setBackground(new Color(240, 240, 240));
-
-        taskPanel.add(createTile("Task 1"));
-        taskPanel.add(createTile("Task 2"));
-        taskPanel.add(createTile("Task 3"));
-        taskPanel.add(createTile("Task 4"));
 
         return taskPanel;
     }
@@ -38,13 +38,18 @@ public class TaskQuest extends JPanel {
         JPanel buttonPanel = new JPanel(new BorderLayout());
         buttonPanel.setPreferredSize(new Dimension(tile.getWidth(), 50));
 
-        JButton greenButton = createButton("+", new Color(51,153,255));
-        JButton redButton = createButton("x", new Color(255, 84, 84, 255));
+        JButton deleteButton = createButton("x", new Color(255, 84, 84, 255));
 
-        //! Apeends the button 
-        buttonPanel.add(redButton, BorderLayout.WEST);
-        buttonPanel.add(greenButton, BorderLayout.EAST);
+        deleteButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent delete){
 
+                taskPanel.remove(tile);
+                taskPanel.revalidate();
+                taskPanel.repaint();
+            }
+        });
+
+        buttonPanel.add(deleteButton, BorderLayout.EAST);
         tile.add(buttonPanel, BorderLayout.SOUTH);
 
         return tile;
@@ -59,22 +64,35 @@ public class TaskQuest extends JPanel {
         return button;
     }
 
-    private void playerBar(){
+    private JPanel playerBar(){
         JPanel panel = new JPanel(new BorderLayout());
 
-        JButton playerButton = createButton(":)", new Color(12,153,255));
+        JButton playerButton = createButton("O", new Color(12,153,255));
         JButton addTask = createButton("+", new Color(51,153,255));
+
+        addTask.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent addTask){
+
+                String task = JOptionPane.showInputDialog(panel, "Testing", null);
+
+                taskPanel.add(createTile(task + "\n"));
+                taskPanel.setLayout(new GridLayout(0, 2, 20, 20));
+                taskPanel.revalidate();
+                taskPanel.repaint();
+            }
+        });
 
         panel.add(playerButton, BorderLayout.WEST);
         panel.add(addTask, BorderLayout.EAST);
 
         panel.setPreferredSize(new Dimension(panel.getWidth(), 50));
-        this.add(panel, BorderLayout.SOUTH);
+        return panel;
     }
 
+    //TODO this could be cleaned up a lot more
     public static void main(String[] args) {
 
-        //? What is this doing?
+        //? What is this doing
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Tile Tasks");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
